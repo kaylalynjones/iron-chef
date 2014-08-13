a7d389b898248cd08eb44f7e114bb940ddadb074
Kayla Jones <KaylaLyne424@gmail.com>
2014-08-13 11:25:49 -0500

    added default data for an empty form

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -1,10 +1,10 @@
 'use strict';
 
 function Recipe(obj){
-  this.name        = obj.name;
-  this.img         = obj.img;
-  this.ingredients = obj.ingredients.split(',').map(function(i){return i.trim();});
-  this.directions  = obj.directions;
+  this.name        = (obj.name.length > 0) ? obj.name : 'Awesome Sauce';
+  this.img         = (obj.img.length > 0) ? obj.img : '/img/recipe_book.jpg';
+  this.ingredients = (obj.ingredients.length > 2) ? obj.ingredients.split(',').map(function(i){return i.trim();}) : ['Awesome', 'Sauce', 'Salt', 'Pepper', 'Garlic', 'Onion Powder', 'Cayenne'];
+  this.directions  = (obj.directions > 0) ? obj.directions : 'Mix it all together, and eat it!';
   this.created     = new Date();
 }
 
